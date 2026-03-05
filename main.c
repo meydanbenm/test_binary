3f953a7b411bc7399832869a9fa7084ba9c2d171
meydanbenm <meydanbenm@gmail.com>
2026-03-05 11:46:57 +0200

    log

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <android/log.h>
+#include <unistd.h>
 
 #define LOG_TAG "XXX"
 
 int main() {
-    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "HELLO WORLD!!!!@@@@@@1111");
+__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "HELLO WORLD sleep");
+    sleep(10);
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "HELLO WORLD wake up");
     return 0;
 }
